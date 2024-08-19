0c884aa7b6ed3e19633ab3978c94101b6c475e34
sancheet48 <sancheet8baidya@gmail.com>
2024-08-19 23:10:18 +0530

    Added container uri

--- Custom-AI-Interface/UI/catalog_ui/const.py
+++ Custom-AI-Interface/UI/catalog_ui/const.py
@@ -8,10 +8,10 @@ LOG_FILE = os.path.join(LOG_DIR, "custom_chatbot.log")
 os.makedirs(LOG_DIR, exist_ok=True)
 
 # ---------------- Chatbot Parameters ----------------
-GENERIC_CHATBOT_URI = os.environ.get("GENERIC_CHATBOT_URI", "")
-RAG_CHATBOT_URI = os.environ.get("RAG_CHATBOT_URI", "")
-SQL_CHATBOT_URI = os.environ.get("SQL_CHATBOT_URI", "")
-MONGO_DB_CHATBOT_URI = os.environ.get("MONGO_DB_CHATBOT_URI", "")
+GENERIC_CHATBOT_URI = os.environ.get("GENERIC_CHATBOT_URI", "https://hub.docker.com/repository/docker/sancheet/generic_chatbot/general")
+RAG_CHATBOT_URI = os.environ.get("RAG_CHATBOT_URI", "https://hub.docker.com/repository/docker/sancheet/rag_chat_with_docs/general")
+SQL_CHATBOT_URI = os.environ.get("SQL_CHATBOT_URI", "https://hub.docker.com/repository/docker/sancheet/sql_chatbot_api/general")
+MONGO_DB_CHATBOT_URI = os.environ.get("MONGO_DB_CHATBOT_URI", "https://hub.docker.com/repository/docker/sancheet/mongo_rag_chatbot/general")
 
 MISTRAL_URI = os.environ.get("MISTRAL_URI", "https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.3")
 LLAMA_URI = os.environ.get("LLAMA_URI", "https://huggingface.co/meta-llama/Meta-Llama-3.1-8B")
